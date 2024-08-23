65b4dbfde0a0da7e8688938687a63bb5a9d02bfb
tomafoks <tomafoks@mail.ru>
2024-08-23 21:56:21 +0500

    optimized the code

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,26 +6,20 @@ public class ArrayStorage {
     int size = 0;
 
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) break;
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                size++;
-                break;
-            }
-        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid) {
+            if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
         }
@@ -33,13 +27,11 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                break;
-            }
-            if (storage[i].uuid == uuid) {
-                storage[i] = storage[size() - 1];
-                storage[size() - 1] = null;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
             }
         }
     }
@@ -48,22 +40,12 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] getResume = new Resume[size()];
-        for (int i = 0; i < size(); i++) {
-            if (storage[i] != null) {
-                getResume[i] = storage[i];
-            }
-        }
-        return getResume;
+        Resume[] resumes = new Resume[size];
+        System.arraycopy(storage, 0, resumes, 0, size);
+        return resumes;
     }
 
     int size() {
         return size;
-//        int count = 0;
-//        for (Resume resume : storage) {
-//            if (resume == null) break;
-//            count++;
-//        }
-//        return count;
     }
 }
