595d3efb80dfa118f3f28ebd053cb4d20c865076
tomafoks <tomafoks@mail.ru>
2024-09-15 01:02:23 +0500

    moved the element to the AbstractArrayStorage class

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -50,6 +50,7 @@ public abstract class AbstractArrayStorage implements Storage {
         } else {
             deleteResume(index);
             size--;
+            storage[size] = null;
         }
     }
 
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -15,7 +15,6 @@ public class ArrayStorage extends AbstractArrayStorage {
     @Override
     protected void deleteResume(int index) {
         storage[index] = storage[size - 1];
-        storage[size - 1] = null;
     }
 
     public int getIndex(String uuid) {
