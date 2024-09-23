85bcf9da23d1371368c47d18ef00c41c21cdbd40
tomafoks <tomafoks@mail.ru>
2024-09-23 18:53:16 +0500

    add other methods in AbstractArrayStorageTest

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -1,5 +1,6 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.Resume;
 import org.junit.Assert;
@@ -17,9 +18,11 @@ public abstract class AbstractArrayStorageTest {
     private static final String uuid_1 = "uuid1";
     private static final String uuid_2 = "uuid2";
     private static final String uuid_3 = "uuid3";
+    private static final String uuid_4 = "uuid4";
     private static final Resume resume_1 = new Resume(uuid_1);
     private static final Resume resume_2 = new Resume(uuid_2);
     private static final Resume resume_3 = new Resume(uuid_3);
+    private static final Resume resume_4 = new Resume(uuid_4);
 
     @Before
     public void setUp() {
@@ -42,20 +45,31 @@ public abstract class AbstractArrayStorageTest {
         Assert.assertTrue(resume_1 == storage.get(resume_1.getUuid()));
     }
 
-    @Test
+    @Test(expected = ExistStorageException.class)
     public void save() {
+        storage.save(resume_4);
+        storage.save(resume_4);
+        Assert.assertTrue(resume_4 == storage.get(resume_4.getUuid()));
     }
 
-    @Test
+    @Test(expected = NotExistStorageException.class)
     public void delete() {
+        storage.delete(resume_3.getUuid());
+        storage.get(resume_3.getUuid());
     }
 
-    @Test
+    @Test(expected = NotExistStorageException.class)
     public void clear() {
+        storage.clear();
+        storage.get(resume_1.getUuid());
     }
 
     @Test
     public void getAll() {
+        storage.getAll();
+        storage.get(resume_1.getUuid());
+        storage.get(resume_2.getUuid());
+        storage.get(resume_3.getUuid());
     }
 
     @Test
