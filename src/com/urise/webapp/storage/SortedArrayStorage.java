9f0ac501aee0437715d887e523ff26d9ab4ef661
tomafoks <tomafoks@mail.ru>
2024-11-15 03:15:20 +0500

    moved comparator from resum to SortedArrayStorage

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -3,8 +3,17 @@ package com.urise.webapp.storage;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
+
+    private static class ResumeComparator implements Comparator<Resume> {
+        public int compare(Resume o1, Resume o2) {
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
+    }
+
+    private static final ResumeComparator RESUME_COMPARATOR = new ResumeComparator();
 
     @Override
     protected void insertResume(Resume r, int index) {
@@ -23,6 +32,6 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected Object getSearchArray(String uuid) {
         Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 }
