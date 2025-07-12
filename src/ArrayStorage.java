c3e139ee075e72f35430b84ad8262756743df062
tomaoks <tomafoks90@gmail.com>
2025-07-12 14:51:28 +0500

    added methods: clear, delete, getAll, size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,14 +3,20 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
         for (int i = 0; i < storage.length; i++) {
             if (storage[i] == null) {
                 storage[i] = r;
+                size++;
                 break;
             }
         }
@@ -29,16 +35,30 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                break;
+            }
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+            }
+        }
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resume = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            resume[i] = storage[i];
+        }
+        return resume;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
