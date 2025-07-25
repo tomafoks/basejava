09f6e8b045c0e67914338483aa8d0d1cf884b902
tomafoks <tomafoks90@gmail.com>
2025-07-25 21:32:18 +0500

    add junit test

---
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,47 @@
+package com.urise.webapp.storage;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AbstractArrayStorageTest {
+
+    @Before
+    public void setUp() {
+        
+    }
+    
+    @Test
+    public void testClear() {
+
+    }
+
+    @Test
+    public void testDelete() {
+
+    }
+
+    @Test
+    public void testGet() {
+
+    }
+
+    @Test
+    public void testGetAll() {
+
+    }
+
+    @Test
+    public void testSave() {
+
+    }
+
+    @Test
+    public void testSize() {
+
+    }
+
+    @Test
+    public void testUpdate() {
+
+    }
+}
