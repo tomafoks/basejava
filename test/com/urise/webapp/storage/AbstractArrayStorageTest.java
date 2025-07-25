de3b3e95a7883d10ab0ed528fba29de88235fc29
tomafoks <tomafoks90@gmail.com>
2025-07-25 22:37:31 +0500

    add testGet method and setUp method

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -1,15 +1,25 @@
 package com.urise.webapp.storage;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.urise.webapp.model.Resume;
+
 public class AbstractArrayStorageTest {
+    static final Storage storage = new ArrayStorage();
+    private final static String UUID_1 = "uuid1";
+    private final static String UUID_2 = "uuid2";
+    private final static String UUID_3 = "uuid3";
 
     @Before
     public void setUp() {
-        
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
     }
-    
+
     @Test
     public void testClear() {
 
@@ -22,6 +32,11 @@ public class AbstractArrayStorageTest {
 
     @Test
     public void testGet() {
+        Assert.assertEquals(UUID_2, storage.get("uuid2").getUuid());
+    }
+
+    @Test
+    public void testGetNotExist() {
 
     }
 
