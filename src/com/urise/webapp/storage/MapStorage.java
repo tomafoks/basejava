0ed031b5ec782cb75f4ab964382454e2c9c9414f
tomafoks <tomafoks90@gmail.com>
2025-08-03 00:59:41 +0500

    add methods in MapStorage and MapStorageTest

--- src/com/urise/webapp/storage/MapStorage.java
+++ src/com/urise/webapp/storage/MapStorage.java
@@ -1,42 +1,55 @@
 package com.urise.webapp.storage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.urise.webapp.model.Resume;
 
 public class MapStorage extends AbstractStorage {
+    Map<String, Resume> storageMap = new HashMap<>();
 
     @Override
     protected void doInsert(Resume resume, Object searchKey) {
+        storageMap.put((String) searchKey, resume);
     }
 
     @Override
     protected boolean isExist(Object searchKey) {
+        return storageMap.containsKey(searchKey);
     }
 
     @Override
     protected Object getSearchKey(String uuid) {
+        return (String) uuid;
     }
 
     @Override
     protected void doUpdate(Resume resume, Object searchKey) {
+        storageMap.replace((String) searchKey, new Resume("updated"));
     }
 
     @Override
     protected void doDeleted(String uuid, Object searchKey) {
+        storageMap.remove(searchKey);
     }
 
     @Override
     protected Resume doGet(Object searchKey) {
+        return storageMap.get(searchKey);
     }
 
     @Override
     public void clear() {
+        storageMap.clear();
     }
 
     @Override
     public Resume[] getAll() {
+        return storageMap.values().toArray(new Resume[0]);
     }
 
     @Override
     public int size() {
+        return storageMap.size();
     }
 }
---
+++ test/com/urise/webapp/storage/MapStorageTest.java
@@ -0,0 +1,7 @@
+package com.urise.webapp.storage;
+
+public class MapStorageTest extends AbstractArrayStorageTest {
+    public MapStorageTest() {
+        super(new MapStorage());
+    }
+}
